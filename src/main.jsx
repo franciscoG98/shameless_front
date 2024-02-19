31da1f97f1eaf3c8db497006c6533715727c6848
franciscog98 <franciscomanuel.gonzalez3@gmail.com>
2024-02-19 06:53:44 -0300

    feat: clean up, install sass and react router dom and prepare structure for project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// import App from './App.jsx'
+import Home from './views/Home.jsx';
+
+import './assets/sass/global.scss'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
